31e947b8920a3b3ade8974958dadd12ede4352eb
matteo <me@matteostefanini.eu>
2016-04-18 15:05:17 +0200

    add bl

--- app/src/main/java/com/ictech/storelocator/LoginActivity.java
+++ app/src/main/java/com/ictech/storelocator/LoginActivity.java
@@ -1,15 +1,36 @@
 package com.ictech.storelocator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.net.Uri;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.ictech.storelocator.loginfragment.First_fragment;
+
+public class LoginActivity extends Activity
+        implements First_fragment.OnFragmentInteractionListener {
+
+    private static final String FragTag = "firstfragment";
+    FragmentManager fragmentManager = getFragmentManager();
+    First_fragment first_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        if( (first_fragment = (First_fragment) fragmentManager.findFragmentByTag(FragTag)) == null){
+            first_fragment = First_fragment.newInstance();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_frame, first_fragment, FragTag)
+                    .commit();
+        }
+    }
 
+    @Override
+    public void onFragmentInteraction() {
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_frame, )
     }
 }
--- app/src/main/java/com/ictech/storelocator/loginfragment/First_fragment.java
+++ app/src/main/java/com/ictech/storelocator/loginfragment/First_fragment.java
@@ -1,71 +1,55 @@
 package com.ictech.storelocator.loginfragment;
 
+import android.app.FragmentManager;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.ictech.storelocator.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link first_fragment.OnFragmentInteractionListener} interface
+ * {@link First_fragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link first_fragment#newInstance} factory method to
+ * Use the {@link First_fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class first_fragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class First_fragment extends Fragment {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+   private OnFragmentInteractionListener mListener;
 
-    private OnFragmentInteractionListener mListener;
-
-    public first_fragment() {
+    public First_fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment first_fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static first_fragment newInstance(String param1, String param2) {
-        first_fragment fragment = new first_fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+    public static First_fragment newInstance() {
+        First_fragment fragment = new First_fragment();
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_first_fragment, container, false);
+        ImageButton imgBtn = (ImageButton)view.findViewById(R.id.imageButton);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onFragmentInteraction();
+            }
+        });
     }
 
     @Override
@@ -97,6 +81,6 @@ public class first_fragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onFragmentInteraction();
     }
 }
