4d2c2b3f4cddc938f9e16bc01c837989fd4246e8
matteo <me@matteostefanini.eu>
2016-04-18 14:43:10 +0200

    add layout fragment form

--- app/src/main/java/com/ictech/storelocator/loginfragment/first_fragment.java
+++ app/src/main/java/com/ictech/storelocator/loginfragment/first_fragment.java
@@ -68,13 +68,6 @@ public class first_fragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first_fragment, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
