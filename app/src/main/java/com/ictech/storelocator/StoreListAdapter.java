807543177812380c488a2478d7a53c041b4ce3cf
Matteo Stefanini <me@matteostefanini.eu>
2016-04-29 22:26:10 +0200

    Change list UI

--- app/src/main/java/com/ictech/storelocator/StoreListAdapter.java
+++ app/src/main/java/com/ictech/storelocator/StoreListAdapter.java
@@ -2,7 +2,10 @@ package com.ictech.storelocator;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,6 +38,7 @@ public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.View
         public TextView storeIndex;
         public ImageView storeImage;
         public CardView cardView;
+        public ImageView storePhone;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -41,6 +46,7 @@ public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.View
             storeIndex = (TextView) itemView.findViewById(R.id.storeIndex);
             storeImage = (ImageView) itemView.findViewById(R.id.storeImage);
             cardView = (CardView) itemView.findViewById(R.id.placeCard);
+            storePhone = (ImageView) itemView.findViewById(R.id.storePhone);
         }
     }
 
@@ -63,6 +69,23 @@ public class StoreListAdapter extends RecyclerView.Adapter<StoreListAdapter.View
         holder.storeName.setText(negozio.nome);
         holder.storeIndex.setText(negozio.indirizzo);
         holder.storeImage.setImageResource(R.drawable.logo_list);
+        holder.storePhone.setImageResource(R.drawable.phone);
+        holder.storePhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission( view.getContext() , android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    //   here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                view.getContext().startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + negozio.telefono)));
+            }
+        });
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
